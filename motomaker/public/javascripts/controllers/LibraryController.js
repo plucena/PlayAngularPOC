6b5edc2cd5286ac9554e72a32ca54b4cf8903bd0
Percival Lucena <percival@ciandt.com>
2015-02-05 16:37:43 -0200

    Added Angular support

---
+++ motomaker/public/javascripts/controllers/LibraryController.js
@@ -0,0 +1,12 @@
+(function(angular) {
+
+var M4M = angular.module('m4mApp', []);
+
+
+M4M.controller('LibraryController', ['$scope', function($scope) {
+	
+	$scope.title = "Tio Patinhas";
+	
+}]);
+
+})(window.angular);
