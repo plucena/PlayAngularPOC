02a44602891836eec45948d87d649eac30a41214
Percival Lucena <percival@ciandt.com>
2015-02-05 17:19:33 -0200

    Angular call to WS

--- motomaker/public/javascripts/controllers/LibraryController.js
+++ motomaker/public/javascripts/controllers/LibraryController.js
@@ -5,7 +5,7 @@ var M4M = angular.module('m4mApp', []);
 
 M4M.controller('LibraryController', ['$scope', function($scope) {
 	
-	$scope.title = "Tio Patinhas";
+	$scope.books  = [{"id":1,"author":"Douglas Adams","collection":"fiction","other":"","title":"hitchhiker's guide to the galaxy","user":"plucena@gmail.com"},{"id":1,"author":"George Orwell","collection":"fiction","other":"","title":"1984","user":"plucena@gmail.com"}];
 	
 }]);
 
