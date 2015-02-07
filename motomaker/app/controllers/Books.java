1799253a8eaca61a7627fecde77b22fe46addaa1
Percival Lucena <percival@ciandt.com>
2015-02-07 19:17:25 -0200

    filter for specific user

--- motomaker/app/controllers/Books.java
+++ motomaker/app/controllers/Books.java
@@ -16,13 +16,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Books  extends Controller {
 	
-	  public static Result index() {
+	  public static Result listAll() {
 		  JsonNode jsonNode = null;
 		  
 		  try {
 			  // get book list from database
 			  List<Book> lista =  Ebean.createQuery(Book.class).findList();
-
+			  
 			  ObjectMapper mapper = new ObjectMapper(); 
 			  String json = mapper.writeValueAsString(lista);
 			   jsonNode = mapper.readTree(json);
@@ -35,5 +35,30 @@ public class Books  extends Controller {
 	      return jsonResult;
 	    }
 
+
+	  public static Result selectByUser(String user) {
+		  JsonNode jsonNode = null;
+		  
+		  try {
+			  // get book list from database
+			  List<Book> lista =   Ebean.find(Book.class).where().like("user", user).findList();
+				
+			  ObjectMapper mapper = new ObjectMapper(); 
+			  String json = mapper.writeValueAsString(lista);
+			   jsonNode = mapper.readTree(json);
+		  }
+		  catch(Exception e){
+			  e.printStackTrace();
+		  }
+		 
+		  Result jsonResult = ok(jsonNode);
+	      return jsonResult;
+	    }
+
+	  
+	  public static Result create() {
+		  return null;
+	  }
+	  
 
 }
---
+++ motomaker/conf/evolutions/default/1.sql
@@ -0,0 +1,26 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table book (
+  id                        integer auto_increment not null,
+  author                    varchar(255),
+  collection                varchar(255),
+  other                     varchar(255),
+  title                     varchar(255),
+  user                      varchar(255),
+  constraint pk_book primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table book;
+
+SET FOREIGN_KEY_CHECKS=1;
+
