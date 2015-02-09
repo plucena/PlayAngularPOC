9939d4de3fbd2b89639740978acfde765c7eed99
Percival Lucena <percival@ciandt.com>
2015-02-09 09:25:03 -0200

    internet server

--- motomaker/public/javascripts/controllers/LibraryController.js
+++ motomaker/public/javascripts/controllers/LibraryController.js
@@ -5,7 +5,7 @@ var M4M = angular.module('m4mApp', []);
 M4M.controller('LibraryController', ['$scope', '$http', function($scope,$http) {
 	
 	
-	$http.get('http://localhost:9000/Book/webresources/com.percivallucena.book.book').then(function(result) {
+	$http.get('http://192.241.171.69:8080/Book/webresources/com.percivallucena.book.book').then(function(result) {
 	$scope.books = result.data;
 	}); 
 	
