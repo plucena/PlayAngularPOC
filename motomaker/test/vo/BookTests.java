44d83b3cdf79f5534ae34d84aabb141db595df15
Mateus da Luz Leonardi <mleonardi@010929CPS.cit-debian>
2015-02-13 09:04:45 -0200

    Todos os testes rodando

--- motomaker/test/vo/BookTests.java
+++ motomaker/test/vo/BookTests.java
@@ -17,56 +17,86 @@ import exception.VOException;
 public class BookTests {
 	
 	@Rule public ExpectedException thrown = ExpectedException.none();
-
-	private BookDAO _bookDAO;
 	
 	@Before
 	public void setUp() {
 		start(fakeApplication(inMemoryDatabase()));
-		
-		_bookDAO = new BookDAO();
 	}
 	
 	@Test
-	public void BookAuthorIsInvalid() throws PersistenceException {
-		thrown.expect(PersistenceException.class);
+	public void BookAuthorIsRequired() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Author is required");
 		
-		Book book = new Book("", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones", "mleonardi@ciandt.com");
-		
-		_bookDAO.create(book);
+		new Book("", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones", "mleonardi@ciandt.com");
 	}
 	
 	@Test
-	public void BookCollectionIsInvalid() throws PersistenceException {
-		thrown.expect(PersistenceException.class);
+	public void BookCollectionIsRequired() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Collection is required");
 		
-		Book book = new Book("George R. R. Martin", "", "", "A Game of Thrones", "mleonardi@ciandt.com");
-		
-		_bookDAO.create(book);
+		new Book("George R. R. Martin", "", "", "A Game of Thrones", "mleonardi@ciandt.com");
 	}
 	
 	@Test
-	public void BookTitleIsInvalid() throws PersistenceException {
-		thrown.expect(PersistenceException.class);
+	public void BookTitleIsRequired() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Title is required");
 		
-		Book book = new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "", "", "mleonardi@ciandt.com");
-		
-		_bookDAO.create(book);
+		new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "", "", "mleonardi@ciandt.com");
 	}
 	
 	@Test
-	public void BookUserIsInvalid() throws PersistenceException {
-		thrown.expect(PersistenceException.class);
+	public void BookUserIsRequired() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Reader is required");
 		
-		Book book = new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones", "");
-		
-		_bookDAO.create(book);
+		new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones", "");
 	}
 	
 	@Test
-	public void BookIsValid() throws PersistenceException {
-		Book book = new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones", "mleonardi@ciandt.com");
+	public void BookAuthorMaxLength() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Author have limit of 255 characters");
 		
-		_bookDAO.create(book);
+		new Book("George R. R. Martin Agatha Cristie Monteiro Lobato Tia Anastácia Pedro de Alcântara Francisco António João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon da Silva Souza Sales Oliveira da Silva Della", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones", "mleonardi@ciandt.com");
+	}
+	
+	@Test
+	public void BookCollectionMaxLength() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Collection have limit of 255 characters");
+		
+		new Book("George R. R. Martin", "Collection of The Best Books of the Entire World George R. R. Martin Agatha Cristie Monteiro Lobato Tia Anastácia Pedro de Alcântara Francisco António João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bou", "", "A Game of Thrones", "mleonardi@ciandt.com");
+	}
+	
+	@Test
+	public void BookOtherMaxLength() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Other have limit of 255 characters");
+		
+		new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "Other Agatha Cristie Monteiro Lobato Tia Anastácia Pedro de Alcântara Francisco António João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon da Silva Souza Sales Oliveira da Silva Dellacour from Tooo", "A Game of Thrones", "mleonardi@ciandt.com");
+	}
+	
+	@Test
+	public void BookTitleMaxLength() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Title have limit of 255 characters");
+		
+		new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones Agatha Cristie Monteiro Lobato Tia Anastácia Pedro de Alcântara Francisco António João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon da Silva Souza Sales Oliveira da Silva Dellaco", "mleonardi@ciandt.com");
+	}
+	
+	@Test
+	public void BookReaderMaxLength() throws VOException {
+		thrown.expect(VOException.class);
+		thrown.expectMessage("Reader have limit of 255 characters");
+		
+		new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones", "mleonardi@ciandt.com Agatha Cristie Monteiro Lobato Tia Anastácia Pedro de Alcântara Francisco António João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon da Silva Souza Sales Oliveira da Silva Dell");
+	}
+	
+	@Test
+	public void BookIsValid() throws VOException {
+		new Book("George R. R. Martin", "As Crônicas de Gelo e Fogo", "", "A Game of Thrones", "mleonardi@ciandt.com");
 	}
 }
